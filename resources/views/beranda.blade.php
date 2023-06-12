d582dd663bfb660f37d012696dddbd748519ab3b
saedhasnuddin <musaedhasanuddin5@gmail.com>
2023-06-12 22:43:23 +0700

    first commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    <script src="../assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.112.5">
+    <title>Headers · Bootstrap v5.3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+
+
+
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="headers.css" rel="stylesheet">
+</head>
+
+<body>
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="check2" viewBox="0 0 16 16">
+            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+        </symbol>
+        <symbol id="circle-half" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
+        </symbol>
+        <symbol id="moon-stars-fill" viewBox="0 0 16 16">
+            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+            <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
+        </symbol>
+        <symbol id="sun-fill" viewBox="0 0 16 16">
+            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+        </symbol>
+    </svg>
+
+    <!-- container -->
+    <main>
+        <h1 class="visually-hidden">Headers examples</h1>
+        <div class="container-fluid bg-info">
+            <header class="d-flex flex-wrap justify-content-center py-3 border-bottom ">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32">
+                        <use xlink:href="" />
+                    </svg>
+                    <img src="{{ asset('storage/gambar/logo.png') }}" width="50" height="50" alt="Logo">
+                </a>
+
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="#" class="nav-link text-white" aria-current="page">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-white">Tentang kami</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-white">menu</a></li>
+                </ul>
+                <div class="text-end">
+                    <button type="button" class="btn btn-primary ">Login</button>
+                    <button type="button" class="btn btn-primary">Sign-up</button>
+                </div>
+            </header>
+        </div>
+
+        <!-- carosel -->
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('storage/gambar/menuu.jpg') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h4>Rice Three</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end carosel -->
+
+
+        <!-- container -->
+        <div class="container mt-5">
+        <h2>Produk</h2>
+        <div class="row text-center mt-5">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('storage/gambar/gorengg.jpeg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">nasi goreng</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('storage/gambar/kuning.jpeg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">nasi kuning</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card" style="width: 20rem;">
+                        <img src="{{ asset('storage/gambar/lemak.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">nasi lemak</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <!-- group layanan -->
+        <div class="container mt-5">
+            <h2>Riview</h2>
+            <div class="row text-center mt-5">
+                <div class="col-lg-4">
+                    <img src="storage/gambar/rizki.jpeg" class="rounded-circle" width="100" height="100">
+                    </img>
+                    <h2 class="fw-normal">"Enak banget"</h2>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="storage/gambar/rizkii.jpeg" class="rounded-circle" width="100" height="100">
+                    </img>
+                    <h2 class="fw-normal">"Udah murah enak lagi"</h2>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img src="storage/gambar/rizkiii.jpeg" class="rounded-circle" width="100" height="100">
+                    </img>
+                    <h2 class="fw-normal">"lejat dan bergizi"</h2>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+        </div>
+        <!-- end group layanan-->
+
+
+        <div class="b-example-divider"></div>
